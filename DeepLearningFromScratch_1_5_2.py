5eb887be6659ae59be0ac83efa56de2951fe8392
limhuijin <gabriel227@naver.com>
2024-01-23 00:25:33 +0900

    day1

---
+++ DeepLearningFromScratch_1_5_2.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+x = np.array([1.0, 2.0, 3.0])
+print(x)
+type(x)
+
+x = np.array([1.0, 2.0, 3.0])
+y = np.array([2.0, 4.0, 6.0])
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+
+A = np.array([[1, 2], [3, 4]])
+print(A)
+print(A.shape)
+
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[3, 0], [0, 6]])
+print(A + B)
+
+A = np.array([[1, 2], [3, 4]])
+B = np.array([10, 20])
+print(A * B)
+
+X = np.array([[51, 55], [14, 19], [0, 4]])
+print(X)
+print(X[0])
+print(X[0][1])
+
+X = X.flatten()         # 1차원 배열로 변환
+print(X)
+
+X[np.array([0, 2, 4])]  # X 배열의 0, 2, 4 인덱스의 원소 얻기
+print(X [X > 15])       # 15이상인 값만 출력하기
