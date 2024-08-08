200438344fd57e5ebaeeae862caa40d0399d4a5b
gabriel227 <gabriel227@naver.com>
2024-08-08 13:49:53 +0900

    2024.08.08 최종 커밋

--- Deep_Learning_From_Scratch_3_2.py
+++ Deep_Learning_From_Scratch_3_2.py
@@ -26,4 +26,8 @@ plt.show()
 def relu(x):
     return np.maximum(0, x)
 
-
+x = np.arange(-5.0, 5.0, 0.1)
+y = relu(x)
+plt.plot(x, y)
+plt.ylim(-1.0, 6.0)
+plt.show()
--- Deep_Learning_From_Scratch_3_4.py
+++ Deep_Learning_From_Scratch_3_4.py
@@ -1,31 +1,36 @@
 import numpy as np
 
-X = np.array([1.0, 0.5])
-W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
-B1 = np.array([0.1, 0.2, 0.3])
-
-print(W1.shape)
-print(X.shape)
-print(B1.shape)
-
-A1 = np.dot(X, W1) + B1
-print(A1)
-
 def sigmoid(x): #시그모이드 함수
     return 1 / (1 + np.exp(-x))
 
-Z1 = sigmoid(A1)
+def identity_function(x): #항등 함수    
+    return x
 
-print(A1)
-print(Z1)
+def init_network():
+    network = {}
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network['b1'] = np.array([0.1, 0.2, 0.3])
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network['b2'] = np.array([0.1, 0.2])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['b3'] = np.array([0.1, 0.2])
 
-W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
-B2 = np.array([0.1, 0.2])
+    return network
 
-print(Z1.shape)
-print(W2.shape)
-print(B2.shape)
+def forward(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
 
-A2 = np.dot(Z1, W2) + B2
-Z2 = sigmoid(A2)
-print(Z2)
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = identity_function(a3)
+
+    return y
+
+network = init_network()
+x = np.array([1.0, 0.5])
+y = forward(network, x)
+print(y)
